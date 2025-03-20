0b248f6483c6e49bff7adcc4cf5d9980b06d04f1
vbacho <victorbachour@gmail.com>
2025-03-20 15:05:38 -0700

    works on both my datasets

--- linear regression.py
+++ linear regression.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-data = pd.read_csv('data.csv')
+data = pd.read_csv('data1.csv')
 # original graph
+plt.figure(1)
 plt.scatter(data.iloc[:,0], data.iloc[:,1])
 plt.xlabel(data.columns[0])
 plt.ylabel(data.columns[1])
 plt.title(f"{data.columns[1]} vs {data.columns[0]}")
-plt.show()
+
 
 def loss_function(m, b, points):
     total_error = 0
@@ -30,24 +31,23 @@ def gradient_descent(m_curr, b_curr, points, lr):
         b_gradient += -(2/n) * (y - (m_curr * x + b_curr))
 
     m = m_curr - m_gradient * lr
-    b = b_curr - m_gradient * lr
+    b = b_curr - b_gradient * lr
 
     return m,b
 
 
 m = 0
 b = 0
-lr = .0001
-
-epochs = 500
+lr = .00000001
+epochs = 300
 
 for i in range(epochs):
-    m, b = gradient_descent(m,b, data, lr)
-
+    m, b = gradient_descent(m, b, data, lr)
+print(m, b)
+plt.figure(2)
 plt.scatter(data.iloc[:, 0], data.iloc[:, 1], color='red')
-x_vals = list(range(min(data.iloc[:, 0]), max(data.iloc[:, 0])))
-y_vals = [m * x + b for x in x_vals]
-
+x_vals = np.linspace(min(data.iloc[:, 0]), max(data.iloc[:, 0]), 100)
+y_vals = m * x_vals + b
 plt.plot(x_vals, y_vals, color='blue')
 plt.xlabel(data.columns[0])
 plt.ylabel(data.columns[1])
